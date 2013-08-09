b8c710fb55f55c9b0bfcf9df79560e97a6aa756f
cokeefe <chaemobile@gmail.com>
2013-08-08 17:43:52 -0700

    validations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,6 @@
 class Skill < ActiveRecord::Base
+  validates_uniqueness_of :name, :context
+
   has_many :ratings
   has_many :users, through: :ratings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :name, :email
+
   has_many :ratings
   has_many :skills, through: :ratings
 end
